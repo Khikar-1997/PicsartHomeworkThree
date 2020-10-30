58ce8abc80f2ff1b14ba884d965801609ae95b7a
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-10-30 10:19:06 +0400

    1_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Task 1
-        //Create an array and fill it with 2 number.
-        int[] array = {2, 2, 2, 2, 2, 2, 2};
-        for (int i = 0; i < array.length; i++) {
+        //Task 2
+        //Create an array and fill it with numbers from 1:1000.
+        int[] array = new int[1001];
+        for (int i = 1; i < array.length; i++) {
+            array[i] = i;
             System.out.print(array[i] + ",");
-        }
-        System.out.println("\n      OR");
-
-        int[] array1 = new int[10];
-        for (int i = 0, j = 2; i < array1.length; i++) {
-            array1[i] = j;
-            System.out.print(array1[i] + ",");
         }
     }
 }
