fdea335e877a6aa655a1492bc6a962fc3bb38989
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-10-30 10:22:16 +0400

    1_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //Task 2
         //Create an array and fill it with numbers from 1:1000.
+
         int[] array = new int[1001];
 
         for (int i = 1; i < array.length; i++) {
