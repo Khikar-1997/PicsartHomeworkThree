0cbc0ef15772784d70121612bea800754c70436e
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-11-01 17:27:33 +0400

    1_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Task 3
-        //Create an array and fill it with odd numbers from -20:20
+        //Task 4
+        //Create an array and fill it. Print all elements which can be divided by 5.
 
-        int countOfOddNumbers = 0;
+        int[] array = {15, 17, 109, 100, 187, 165, 145};
 
-        for (int i = -19; i < 20; i += 2) {
-            countOfOddNumbers++;
-        }
-
-        int[] array = new int[countOfOddNumbers];
-
-        for (int j = 0, i = -19; j < array.length; i += 2, j++) {
-            array[j] = i;
-            System.out.print(array[j] + ",");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 5 == 0){
+                System.out.print(array[i] + ",");
+            }
         }
     }
 }
