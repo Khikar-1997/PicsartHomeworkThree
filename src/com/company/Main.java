6e0f4724e0b9f134ce6a34bd90f9798b60c12c5b
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-11-02 01:06:52 +0400

    1_7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Task 7
-        //Given an integer, 0< N < 21 , print its first 10 multiples.
-        //Each multiple N x i (where 0<i<11) should be printed on a new line in the form: N x i = result.
+        int[] array = new int[10000];
 
-        int number = 5;
-        int result;
-        if (number > 0 && number < 21) {
-            for (int i = 1; i < 11; i++) {
-                result = number * i;
-                System.out.println(number + " x " + i + " = " + result);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0, k = array[i]; j < 1000; j++, k /= 2) {
+                if (k % 2 == 0) {
+                    if (k == 2) {
+                        System.out.print(array[i] + ",");
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\n                     OR");
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (array[i] != 0 && (array[i] & (array[i] - 1)) == 0) {
+                    System.out.print(array[i] + ",");
+                    break;
+                }
             }
         }
     }
