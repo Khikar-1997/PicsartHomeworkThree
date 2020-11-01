9b79aa3fefc44467966c834eb79d931841a20966
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-11-01 17:35:21 +0400

    1_6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Task 5
-        //Create an array and fill it. Print all elements which are between 24.12 and 467.23.
+        //Task 6
+        //Create an array and fill it. Print count of elements which can be divided by 2.
 
-        double[] array = new double[500];
+        int[] array = new int[100];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 24.12 && array[i] < 467.23) {
+            if (array[i] % 2 == 0) {
                 System.out.print(array[i] + ",");
             }
         }
