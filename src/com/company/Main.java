17ce63e07a12ee234c7fee11a04b2ee0f35d9f92
Khikar-1997 <harutyunyan.xikar@mail.ru>
2020-11-04 08:42:14 +0400

    1_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,34 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] array = new int[10000];
+        int[] array = {3, 5, 1, 2, 7, 5};
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = i;
-        }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0, k = array[i]; j < 1000; j++, k /= 2) {
-                if (k % 2 == 0) {
-                    if (k == 2) {
-                        System.out.print(array[i] + ",");
-                        break;
-                    }
-                } else {
-                    break;
+            int k;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    k = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = k;
                 }
             }
         }
 
-        System.out.println("\n                     OR");
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < 1000; j++) {
-                if (array[i] != 0 && (array[i] & (array[i] - 1)) == 0) {
-                    System.out.print(array[i] + ",");
-                    break;
-                }
-            }
+        for (int j : array) {
+            System.out.println(j);
         }
     }
 }
